47233421089bf047d7e1a1a2cb42ed7e4c20d6ee
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2025-05-07 15:31:34 -0400

    adicionado página inicial da aplicação

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Obras
 
-# Create your views here.
+def home(request):
+    print("Renderizando home.html")
+    obras = Obras.objects.all()
+    return render(request, 'core/home.html', {'obras': obras})
