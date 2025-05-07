8f53476af1015da68cf64201a6a87d11c8670f8e
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2025-05-07 15:00:58 -0400

    Adicionados os arquivos em admin para register

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Obras, Material, InsumoUsado
 
-# Register your models here.
+admin.site.register(Obras)
+admin.site.register(Material)
+admin.site.register(InsumoUsado)
