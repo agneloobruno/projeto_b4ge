7247bef4973d6f18e4bada478e88ac30e7491855
agneloobruno <brunoagnelo.almeida@gmail.com>
2025-06-29 23:32:22 -0400

    Feat: adiciona testes para criação de obras e implementa migração para renomear e remover campos no modelo Obras

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from core.models import Obras
 
-# Create your tests here.
+class ObraTestCase(TestCase):
+    def test_criacao_obra(self):
+        obra = Obras.objects.create(
+            nome="Teste Residencial",
+            tipologia="Residencial",
+            estado="MT",
+            cidade="Cuiabá",
+            area_total_construir=120.5
+        )
+        self.assertEqual(obra.nome, "Teste Residencial")
+        self.assertEqual(obra.tipologia, "Residencial")
+        self.assertEqual(obra.estado, "MT")
+        self.assertEqual(obra.cidade, "Cuiabá")
+        self.assertAlmostEqual(obra.area_total_construir, 120.5)
